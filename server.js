1907302b86b955102442e0bcd2481cf43a353302
pcion <patrick.ciongoli@gmail.com>
2022-03-09 19:13:03 -0500

    commit

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
-const app = express;
+const { animals } = require('./data/animals');
+
+
+const app = express();
+
+
+// add route
+app.length('/api/animals', (req, res) => {
+    let results = animals;
+    console.log(req.query);
+    res.json(animals);
+});
 
 // listens for connections on the specified host and port
 app.listen(3001, () => {
